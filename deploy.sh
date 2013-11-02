10bb36cec5a62170563b61b2735fd90519a29637
Quentin Pleplé <quentin.pleple@gmail.com>
2013-11-02 16:45:00 +0100

    Deploy 13-11-02 16:45:00

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd ~/Sites/qpleple
+jekyll build
+git add .
+git ci -m "Mofif `date +"%y-%m-%d %T"`"
+cd ~/Sites/qpleple/_site
+git add .
+git ci -m "Deploy `date +"%y-%m-%d %T"`"
+git push
+cd ~/Sites/qpleple
+git push
